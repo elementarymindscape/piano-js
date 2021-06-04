e75f7fde52c76a62f5ac7125289f59505690416a
elementarymindscape <sharma.vv13@gmail.com>
2021-06-04 16:17:33 +0530

    added keyboard keys for playing

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+const WHITE_KEYS = ['s', 'd', 'f', 'g', 'h', 'j', 'k']
+const BLACK_KEYS = ['e', 'r', 't', 'y', 'u', 'i']
+
 const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+
 
 keys.forEach( key => {
     key.addEventListener('click', ()=> playNote(key))
+})
+
+document.addEventListener('keydown' , e => {
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if(whiteKeyIndex > -1)  playNote(whiteKeys[whiteKeyIndex])
+    if(blackKeyIndex > -1)  playNote(blackKeys[blackKeyIndex])
+
 })
 
 function playNote (key) {
